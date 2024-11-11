459188f61192e1cac555db60510095c9d68f50e8
Swadesh Kumar <swadesh0287@gmail.com>
2024-11-11 14:08:28 +0530

    updiuuiui

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
-const jsonServer = require("json-server"); // importing json-server library
+const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("index.json");
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 5000; 
+const port = process.env.PORT || 5000;
 
 server.use(middlewares);
 server.use(router);
 
-server.listen(port);
+server.listen(port, () => {
+  console.log(`JSON Server is running on port ${port}`);
+});
